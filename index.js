673d83dc3bf6bc5f1cf2fbfc1e172ed49905e928
ROHITKUCHERIA <51601590+ROHITKUCHERIA@users.noreply.github.com>
2021-03-17 15:58:06 +0530

    Simple Greeting page in React.jsx

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+// import Greeting from './Greeting';
+
+ReactDom.render(
+  <>
+    {/* <Greeting/>, */}
+    <App/>
+  </>
+,document.getElementById('root'));
+
+
+
+
+
+
+
