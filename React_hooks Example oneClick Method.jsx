b4b9aec9e786d2e672927efd69eca700eb8d509e
ROHITKUCHERIA <51601590+ROHITKUCHERIA@users.noreply.github.com>
2021-03-17 15:51:52 +0530

    react_hooks

---
+++ React_hooks Example oneClick Method.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const state = useState();
+
+
+  /*  ** In useState mathod we use array destructuring rule. 
+    In the useState method we have 3 state first one is the current state known as *** count *** and 
+    second is updated values state known as **SetCount**. 
+    last third is the initial value of the function or we can say tha initial value of the state. and This is known as useState. 
+    usestate() give our value to the count variable and now setcount function is called and then value is changes using ** count+1 **.
+  */
+  const [count,setCount ] = useState(0);
+
+  const IncNum = () => {
+    setCount (count+1);
+  };
+
+
+  return (<>
+    <div className="first_div">
+      <h1>{count}</h1>
+      <button className="btn btn-outline-warning" onClick={IncNum} >Click Me </button>
+    </div></>);
+}
+
+export default App;
